fe9537bef571787d85fbd335bf4ac6ba6da06874
FredrikJT <fredrik.j.torneus@gmail.com>
2019-12-04 22:05:34 +0100

    day3

--- day3_2_manhattan.py
+++ day3_2_manhattan.py
@@ -1,7 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 22:56:04 2019
 
+@author: HarrePC2
+"""
+
+#5000,3998
+
+#4639
+
+#1002
 
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open('day3_input1.csv', 'r') as f:
   reader = csv.reader(f)
@@ -22,10 +34,15 @@ with open('in2.csv', 'r') as f:
 
 board = np.zeros((11000,11000),dtype=int)
 
+np_arr1 = np.array([[0, 5000, 5000]])
+
+#np.append(np_arr1, np_arr2, axis=0)
+
 x = 5000
 y = 5000
 xnew = 5000
 ynew = 5000
+steps1 = 0
 
 for i in range(len(input1[0])):
     direction = input1[0][i][0]
@@ -34,24 +51,62 @@ for i in range(len(input1[0])):
     if direction == 'U':
         print('in U:', length)
         xnew = x + length
+        
+        for i in range(length):
+            np_arr1 = np.append(np_arr1, np.array([[steps1+i, x+i, y]]), axis=0)
+            
+            
+            if x+i == 5046:
+                if y == 5146:
+                    print('pos')
           
         board[x:xnew, y] = 1+0*board[x:xnew, y]
+        
+        steps1 = steps1 + length
       
     if direction == 'D':
         print('in D:', length)
         xnew = x - length
+       
+        
+        for i in range(length):
+            np_arr1 = np.append(np_arr1, np.array([[steps1+i, x-i, y]]), axis=0)
+            if x-i == 5046:
+                if y == 5146:
+                    print('pos')
+        
+        steps1 = steps1 + length
       
         board[xnew:x, y] = 1+0*board[xnew:x, y]
       
     if direction == 'L':
         print('in L:', length)
         ynew = y - length
+        
+        
+        
+        for i in range(length):
+            np_arr1 = np.append(np_arr1, np.array([[steps1+i, x, y-i]]), axis=0)
+            if x == 5046:
+                if y-i == 5146:
+                    print('pos')
+
+                
+        steps1 = steps1 + length
       
         board[x, ynew:y] = 1+0*board[x, ynew:y]
       
     if direction == 'R':
         print('in R:', length)
         ynew = y + length
+        
+        for i in range(length):
+            np_arr1 = np.append(np_arr1, np.array([[steps1+i, x, y+i]]), axis=0)
+            if x == 5046:
+                if y+i == 5146:
+                    print('pos')
+                
+        steps1 = steps1 + length
       
         board[x, y:ynew] = 1+0*board[x, y:ynew]
         
@@ -65,6 +120,8 @@ x = 5000
 y = 5000
 xnew = 5000
 ynew = 5000
+steps = 0
+np_arr2 = np.array([[0, 5000, 5000]])
     
 for i in range(len(input2[0])):
     direction = input2[0][i][0]
@@ -73,29 +130,62 @@ for i in range(len(input2[0])):
     if direction == 'U':
         print('in U:', length)
         xnew = x + length
+        
+        for i in range(length):
+            np_arr2 = np.append(np_arr2, np.array([[steps+i, x+i, y]]), axis=0)
+            if board[x+i, y] == 1:
+                print(steps)
+        
+        steps = steps+length
           
         board[x:xnew, y] = 2*board[x:xnew, y]
       
     if direction == 'D':
         print('in D:', length)
         xnew = x - length
+        
+        for i in range(length):
+            np_arr2 = np.append(np_arr2, np.array([[steps+i, x-i, y]]), axis=0)
+            if board[x-i, y] == 1:
+                print(steps)
+        
+        steps = steps+length
       
         board[xnew:x, y] = 2*board[xnew:x, y]
       
     if direction == 'L':
         print('in L:', length)
         ynew = y - length
+        
+        for i in range(length):
+            np_arr2 = np.append(np_arr2, np.array([[steps+i, x, y-i]]), axis=0)
+            if board[x, y-i] == 1:
+                print(steps)
+        
+        steps = steps+length
       
         board[x, ynew:y] = 2*board[x, ynew:y]
       
     if direction == 'R':
         print('in R:', length)
         ynew = y + length
+        
+        for i in range(length):
+            np_arr2 = np.append(np_arr2, np.array([[steps+i, x, y+i]]), axis=0)
+            if board[x, y+i] == 1:
+                print(steps)
+        
+        steps = steps+length
       
         board[x, y:ynew] = 2*board[x, y:ynew]
         
     x = xnew
     y = ynew
+    
+    
+plt.plot(np_arr1[:,1], np_arr1[:,2], 'r')
+plt.plot(np_arr2[:,1], np_arr2[:,2], 'b')
+plt.show()
     
 best = 10000
 for i in range(10000):
